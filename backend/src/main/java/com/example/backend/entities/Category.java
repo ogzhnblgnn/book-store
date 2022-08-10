f5ef3df5049f43ab13c737ca774c488b3a2dc795
Oğuzhan Bilgin <ogzhnbxx@gmail.com>
2022-08-10 12:53:09 +0300

    CategoryEntity has been created.

---
+++ backend/src/main/java/com/example/backend/entities/Category.java
@@ -0,0 +1,26 @@
+package com.example.backend.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    
+    @Id
+    @GeneratedValue
+    @Column(name="category_id")
+    private int id;
+    @Column(name="category_name")
+    private String categoryName;
+}
