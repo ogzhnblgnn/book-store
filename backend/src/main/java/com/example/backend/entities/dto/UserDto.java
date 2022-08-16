bda9ff1ef7b3d6496370586fc688f7a1300fd8ac
Oğuzhan Bilgin <ogzhnbxx@gmail.com>
2022-08-16 16:53:12 +0300

    UserDto has been completed.

---
+++ backend/src/main/java/com/example/backend/entities/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.example.backend.entities.dto;
+
+import java.util.Set;
+
+import com.example.backend.entities.Role;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private Set<Role> roles;
+}
