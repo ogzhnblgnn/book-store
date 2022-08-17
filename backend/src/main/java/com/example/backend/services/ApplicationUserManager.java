76f8aa9b97c10c7153b19c6cebb85298fae992be
Oğuzhan Bilgin <ogzhnbxx@gmail.com>
2022-08-17 16:37:31 +0300

    @Service has been added.

--- backend/src/main/java/com/example/backend/services/ApplicationUserManager.java
+++ backend/src/main/java/com/example/backend/services/ApplicationUserManager.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.backend.services.Abstract.ApplicationUserDao;
 
+@Service
 public class ApplicationUserManager implements UserDetailsService {
 
     private final ApplicationUserDao applicationUserDao;
