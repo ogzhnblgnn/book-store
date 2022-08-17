21545c3ddb58155c228139d176ddae543c4b7c02
Oğuzhan Bilgin <ogzhnbxx@gmail.com>
2022-08-17 17:49:55 +0300

    Auhtor preAuth has been added.

--- backend/src/main/java/com/example/backend/controllers/AuthorController.java
+++ backend/src/main/java/com/example/backend/controllers/AuthorController.java
@@ -2,6 +2,7 @@ package com.example.backend.controllers;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,16 +38,19 @@ public class AuthorController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('author:post')")
     public ApiResponse<Author> addAuthor(@RequestBody Author author){
         return authorService.addAuthor(author);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('author:put')")
     public ApiResponse<Author> updateAuthor(@PathVariable(name="id", required = true) int id, @RequestBody Author author){
         return authorService.updateAuthor(id, author);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('author:delete')")
     public ApiResponse<?> deleteAuthor(@PathVariable(name="id", required = true) int id){
         return authorService.deleteAuthor(id);
     }
